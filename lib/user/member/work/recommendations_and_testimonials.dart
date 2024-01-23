423bae04f269a0b28d4ccbeaa9c36185177071f6
Anirudh Sharma <mcxiv14@gmail.com>
2024-01-23 23:36:15 +0530

    Added recommendations screen

---
+++ lib/user/member/work/recommendations_and_testimonials.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:kiit_connect/theme/ui_builder.dart';
+import 'package:kiit_connect/user/member/work/projects.dart';
+
+class RecommendationsAndTestimonials extends StatelessWidget {
+  const RecommendationsAndTestimonials({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FormBuilder("Recommendations", (c) => PersonalProject())
+        .addTextField("Recommender's Name")
+        .addTextField("Relationship")
+        .addTextField("Testimonial Text", maxLines: 3)
+        ;
+  }
+}
